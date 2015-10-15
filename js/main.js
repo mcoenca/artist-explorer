95b645ffcc53ae6ef2e5352b952b480d9db9cf45
mcoenca <m.coenca@gmail.com>
2015-10-15 15:36:31 +0200

    fixing stuff

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@
     }
 
     function toTitleCase(str) {
-        return str.replace(/\w\S*/g, function (txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        return str ? str.replace(/\w\S*/g, function (txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); }) : "";
     }
 
     //get id from some way, or id of sharing
